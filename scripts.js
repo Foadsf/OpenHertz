5a7d5f79655beae5dbf62d25dc71753218d65670
Foad Sojoodi Farimani <f.s.farimani@gmail.com>
2021-02-28 21:59:43 +0100

    towards calculations

--- scripts.js
+++ scripts.js
@@ -8,26 +8,31 @@ function contactTypeFun(inputOption) {
         // document.getElementById('contactType').src = "pics/CylinderCylinderPerpendicular.PNG";
         orientationFun(document.getElementById("OrientationSelector").value);
         document.getElementById("secondRadius").style.display = "block";
+        document.getElementById("effectiveRadius").style.display = 'table-row';
     } else {
         document.getElementById("orientationList").style.display = "none";
         switch (inputOption) {
             case "1":
                 document.getElementById('contactType').src = "pics/SpherePlane.PNG";
                 document.getElementById("secondRadius").style.display = "none";
+                document.getElementById("effectiveRadius").style.display = "none";
                 // console.log("one");
                 break;
             case "2":
                 document.getElementById('contactType').src = "pics/SphereSphere.PNG";
                 document.getElementById("secondRadius").style.display = "block";
+                document.getElementById("effectiveRadius").style.display = 'table-row';
                 // console.log("two");
                 break;
             case "3":
                 document.getElementById('contactType').src = "pics/CylinderPlane.PNG";
                 document.getElementById("secondRadius").style.display = "none";
+                document.getElementById("effectiveRadius").style.display = "none";
                 // console.log("three");
                 break;
         }
     }
+    calculateEffectiveRadius()
 }
 
 function orientationFun(inputOption) {
@@ -39,4 +44,25 @@ function orientationFun(inputOption) {
             document.getElementById('contactType').src = "pics/CylinderCylinderParallel.PNG";
             break;
     }
+}
+
+function calculateEffectiveRadius() {
+    var fradius = parseFloat(document.getElementById("firstRadius").value);
+    var sradius = parseFloat(document.getElementById("secondRadiusValue").value);
+    var calculationResult = fradius * sradius / (fradius + sradius);
+    document.getElementById("effectiveRadiusValue").innerHTML = calculationResult.toFixed(2);
+}
+
+function calculateEffectiveElasticity() {
+    var felastic = parseFloat(document.getElementById("firstElastic").value);
+    var selastic = parseFloat(document.getElementById("secondElastic").value);
+    var fPoisson = parseFloat(document.getElementById("firstPoisson").value);
+    var sPoisson = parseFloat(document.getElementById("secondPoisson").value);
+    var calculationResult = felastic * selastic / (selastic * (1 - Math.pow(fPoisson, 2)) + felastic * (1 - Math.pow(sPoisson, 2)));
+    document.getElementById("effectiveElasticityValue").innerHTML = calculationResult.toFixed(0);
+}
+
+function updateresults() {
+    calculateEffectiveRadius();
+    calculateEffectiveElasticity()
 }
